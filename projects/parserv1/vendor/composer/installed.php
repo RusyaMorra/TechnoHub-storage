727c7abe699da88bdac7798618070d9c7ed65807
Rusya Morra <80226063+RusyaMorra@users.noreply.github.com>
2022-05-31 22:43:49 +0600

    newborn

---
+++ projects/parserv1/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'parser/siteparser',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'krugozor/database' => array(
+            'pretty_version' => 'V2.0',
+            'version' => '2.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../krugozor/database',
+            'aliases' => array(),
+            'reference' => '836b7ff80604f748910f00d92a35a3ccaab77b0a',
+            'dev_requirement' => false,
+        ),
+        'parser/siteparser' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
