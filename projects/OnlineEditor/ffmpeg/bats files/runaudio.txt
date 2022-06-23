56c9ce82a38756606b8c27fe4641e498ca159fcc
Rusya Morra <80226063+RusyaMorra@users.noreply.github.com>
2022-06-23 11:21:27 +0600

    new

--- projects/OnlineEditor/ffmpeg/bats files/runaudio.txt
+++
@@ -1,1 +0,0 @@
-ffmpeg -i video2.mp4 -vf scale=1920x1080  output.mp4
--- projects/OnlineEditor/startPython.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-shell_exec("C:\Users\comp\AppData\Local\Programs\Python\Python38   C:\Users\comp\Desktop\tech\web\OpenServer\domains\plugin\ffmpeg\app.py");
-   
